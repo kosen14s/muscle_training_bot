0fc2528c0359c645532eaa1a27aeba0662bfec27
yamasy1549 <sanae@yamasy.info>
2016-05-02 19:53:35 +0900

    Collect :muscle: thus far in all channels

--- check_muscle.rb
+++ check_muscle.rb
@@ -4,31 +4,45 @@ require 'json'
 require 'dotenv'
 require 'pry'
 
+def get_json(url)
+  res = Net::HTTP.get(URI.parse(url))
+  JSON.parse(res)
+end
+
+def valid?(json)
+  json['ok']
+end
+
 Dotenv.load
 
-# TODO: CH一覧もまとめて取得
-CH_GENERAL = 'C0KNQP0ET'
-CH_MUSCLE = 'C157LN04W'
-CH_PROFILE = 'C0KP424FL'
-CH_RONDOM = 'C0KNR3NJ2'
+url = "https://slack.com/api/channels.list?token=#{ENV['TOKEN']}"
+json_ch = get_json(url)
 
 muscle_count = 0
+channels = []
 
-url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{CH_PROFILE}"
+# json_ch.valid? って書きたい
+if valid?(json_ch)
+  json_ch['channels'].each do |ch|
+    channels << [id: ch['id'], name: ch['name']]
+  end
+end
 
-res = Net::HTTP.get(URI.parse(url))
-json = JSON.parse(res)
+channels.each do |channel|
+  url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel[0][:id]}"
+  json = get_json(url)
 
-if json['ok']
-  json['messages'].each do |message|
-    muscle_count += 1 if message['text'].include?(':muscle:')
+  if valid?(json)
+    json['messages'].each do |message|
+      muscle_count += 1 if message['text'].include?(':muscle:')
 
-    if message['reactions']
-      message['reactions'].each do |reaction|
-        muscle_count += reaction['users'].count if reaction['name'] == 'muscle'
+      if message['reactions']
+        message['reactions'].each do |reaction|
+          muscle_count += reaction['users'].count if reaction['name'] == 'muscle'
+        end
       end
     end
   end
-
-  puts muscle_count
 end
+
+puts muscle_count
