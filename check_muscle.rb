8566b1d201b0616f80118a61ce391dcd0f2fffd5
yamasy1549 <sanae@yamasy.info>
2016-05-03 21:13:20 +0900

    Collect :muscle:s since 21:00 the day before

--- check_muscle.rb
+++ check_muscle.rb
@@ -16,10 +16,10 @@ def has_more?(json)
   json['has_more']
 end
 
-def count_muscles(json, beginning_of_the_day, channel_id)
+def count_muscles(json, std_of_the_day, channel_id)
   if valid?(json)
     json['messages'].each do |message|
-      if Time.at(message['ts'].to_i) > beginning_of_the_day
+      if Time.at(message['ts'].to_i) > std_of_the_day
         $muscle_count += message['text'].scan(MUSCLE).size
 
         if message['reactions']
@@ -36,7 +36,7 @@ def count_muscles(json, beginning_of_the_day, channel_id)
       latest = json['messages'].last['ts']
       url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel_id}&latest=#{latest}"
       json = get_json(url)
-      count_muscles(json, beginning_of_the_day, channel_id) if has_more?(json)
+      count_muscles(json, std_of_the_day, channel_id) if has_more?(json)
     end
   end
 
@@ -49,12 +49,12 @@ MUSCLE = "muscle"
 $muscle_count = 0
 channels = []
 
-# 今日がはじまったときの時間、比較用
+# 時間、比較用
 time = Time.now
 year = time.year
 month = time.month
 day = time.day
-beginning_of_the_day = Time.new(year, month, day)
+std_of_the_day = Time.new(year, month, day, 21) - (24 * 60 * 60)
 
 url = "https://slack.com/api/channels.list?token=#{ENV['TOKEN']}"
 json_ch = get_json(url)
@@ -71,7 +71,7 @@ channels.each do |channel|
   url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel[0][:id]}"
   json = get_json(url)
 
-  count_muscles(json, beginning_of_the_day, channel[0][:id])
+  count_muscles(json, std_of_the_day, channel[0][:id])
 end
 
 message = "今日の筋肉は #{$muscle_count} でした。\n筋肉つけていこうな :muscle:"
