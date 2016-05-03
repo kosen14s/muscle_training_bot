bf405d4de4b86c19d61e3cd6073b53afef32830b
yamasy1549 <sanae@yamasy.info>
2016-05-03 15:50:54 +0900

    Enable to get over 100 messages

--- check_muscle.rb
+++ check_muscle.rb
@@ -2,11 +2,6 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'dotenv'
-require 'pry'
-
-MUSCLE = "muscle"
-muscle_count = 0
-channels = []
 
 def get_json(url)
   res = Net::HTTP.get(URI.parse(url))
@@ -17,7 +12,49 @@ def valid?(json)
   json['ok']
 end
 
+def has_more?(json)
+  json['has_more']
+end
+
+def count_muscles(json, beginning_of_the_day, channel_id)
+  if valid?(json)
+    json['messages'].each do |message|
+      if Time.at(message['ts'].to_i) > beginning_of_the_day
+        $muscle_count += message['text'].scan(MUSCLE).size
+
+        if message['reactions']
+          message['reactions'].each do |reaction|
+            $muscle_count += reaction['users'].count if reaction['name'] == MUSCLE
+          end
+        end
+      else
+        break
+      end
+    end
+
+    if has_more?(json)
+      latest = json['messages'].last['ts']
+      url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel_id}&latest=#{latest}"
+      json = get_json(url)
+      count_muscles(json, beginning_of_the_day, channel_id) if has_more?(json)
+    end
+  end
+
+  $muscle_count
+end
+
 Dotenv.load
+
+MUSCLE = "muscle"
+$muscle_count = 0
+channels = []
+
+# 今日がはじまったときの時間、比較用
+time = Time.now
+year = time.year
+month = time.month
+day = time.day
+beginning_of_the_day = Time.new(year, month, day)
 
 url = "https://slack.com/api/channels.list?token=#{ENV['TOKEN']}"
 json_ch = get_json(url)
@@ -29,33 +66,12 @@ if valid?(json_ch)
   end
 end
 
-# 今日がはじまったときの時間、比較用
-time = Time.now
-year = time.year
-month = time.month
-day = time.day
-beginning_of_the_day = Time.new(year, month, day)
-
 # メッセージとreactions取得
 channels.each do |channel|
-  url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel[0][:id]}&inclusive=1"
+  url = "https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel[0][:id]}"
   json = get_json(url)
 
-  if valid?(json)
-    json['messages'].each do |message|
-      if Time.at(message['ts'].to_i) > beginning_of_the_day
-        muscle_count += message['text'].scan(MUSCLE).size
-
-        if message['reactions']
-          message['reactions'].each do |reaction|
-            muscle_count += reaction['users'].count if reaction['name'] == MUSCLE
-          end
-        end
-      else
-        break
-      end
-    end
-  end
+  count_muscles(json, beginning_of_the_day, channel[0][:id])
 end
 
-puts muscle_count
+puts $muscle_count
